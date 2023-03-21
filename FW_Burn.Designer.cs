3311c160110fb27055d4a540da205598c57320a8
Alexey <alexey.tseirif@prospera.ag>
2023-03-21 13:53:52 +0200

    updates

--- FW_Burn.Designer.cs
+++ FW_Burn.Designer.cs
@@ -605,7 +605,7 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(91, 77);
             this.button1.TabIndex = 52;
-            this.button1.Text = "PAIR1 UNIT";
+            this.button1.Text = "PAIR 1 UNIT";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
