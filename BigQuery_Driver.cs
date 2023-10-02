c2348aace61f57ef7f5a47eb3c436f27f2925498
Atrimax <alexey.tseirif@prospera.ag>
2023-10-02 14:32:17 +0300

    updates

---
+++ BigQuery_Driver.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FW_Burn
+{
+    internal class BigQuery_Driver
+    {
+    }
+}
