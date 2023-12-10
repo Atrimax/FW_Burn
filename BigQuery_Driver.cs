312591c30fff4f3348ef1710a916cd5437379f10
Alexey <alexey.tseirif@prospera.ag>
2023-12-10 16:54:02 +0200

    update

--- BigQuery_Driver.cs
+++ BigQuery_Driver.cs
@@ -6,7 +6,21 @@ using System.Threading.Tasks;
 
 namespace FW_Burn
 {
+
+    
     internal class BigQuery_Driver
     {
+
+        readonly string GoogleAPIKeyPath = System.Configuration.ConfigurationManager.ConnectionStrings["GoogleAPIKeyPath"].ToString();
+
+        public void Update_BigQuery_Pairing()
+        {
+
+        }
+
+        public void Save_BigQuery_Pairing()
+        {
+
+        }
     }
 }
